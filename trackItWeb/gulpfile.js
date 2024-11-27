028edbe9af574fb4c2d4f01be24e5e6b1366c44c
souvikDey7 <deysouvik2014@gmail.com>
2024-11-28 00:38:43 +0530

    url changes

--- trackItWeb/gulpfile.js
+++ trackItWeb/gulpfile.js
@@ -6,11 +6,20 @@ gulp.task('copy-assets', function () {
     .pipe(gulp.dest('dist/assets'));
 });
 
-// Example: Process styles (optional)
-gulp.task('process-styles', function () {
-  return gulp.src('src/styles/**/*.css')
+
+const fs = require('fs');
+
+gulp.task('process-styles', function (done) {
+  const stylesPath = 'src/styles/**/*.css';
+  if (!fs.existsSync('src/styles')) {
+    console.log('No styles directory found. Skipping task.');
+    return done();
+  }
+  return gulp.src(stylesPath)
     .pipe(gulp.dest('dist/styles'));
 });
+
+  
 
 // Define the `build` task
 gulp.task('build', gulp.series('copy-assets', 'process-styles'));
--- trackItWeb/src/app/app-routing.module.ts
+++ trackItWeb/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RoomComponent } from './room/room.component';
 
 const routes: Routes = [
-  { path: 'roomCreate', component: RoomComponent }
+  { path: 'joinroom', component: RoomComponent }
 ];
 
 @NgModule({
